ddec1aa2185bdea2fe1d2147dbd8cede425f01ab
TitasRoy14 <roytitas800@gmail.com>
2025-06-11 20:29:53 +0530

    04: authentication ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import { SignInVIew } from '@/modules/auth/ui/views/sign-in-view';
+
+const SignInPage = () => {
+  console.log('Sign in page');
+  return (
+    <div>
+      <SignInVIew />
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { SignUpView } from '@/modules/auth/ui/views/sign-up-view';
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <SignUpView />
+    </div>
+  );
+};
+
+export default SignUpPage;
