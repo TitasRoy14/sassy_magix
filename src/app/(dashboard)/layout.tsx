b34ade481963167f382571ff521d49f0d6f4afc3
TitasRoy14 <roytitas800@gmail.com>
2025-06-14 12:26:47 +0530

    07: dashboard navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,12 +2,14 @@ import { PropsWithChildren } from 'react';
 
 import { SidebarProvider } from '@/components/ui/sidebar';
 import DashboardSidebar from '@/modules/dashboard/ui/components/dashboard-sidebar';
+import DashboardNavbar from '@/modules/dashboard/ui/components/dashboard-navbar';
 
 const DashBoardLayout = ({ children }: PropsWithChildren) => {
   return (
     <SidebarProvider>
       <DashboardSidebar />
       <main className="flex flex-col h-screen w-screen bg-muted">
+        <DashboardNavbar />
         {children}
       </main>
     </SidebarProvider>
