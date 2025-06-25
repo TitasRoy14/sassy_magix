0ddfd18e1546716e87e7c24d8d4b26d00cf153c7
TitasRoy14 <roytitas800@gmail.com>
2025-06-25 14:10:06 +0530

    10: Responsive Dialog

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -8,7 +8,7 @@ export const agentsRouter = createTRPCRouter({
     const data = await db.select().from(agents);
 
     //await new Promise((resolve) => setTimeout(resolve, 5000));
-    throw new TRPCError({ code: 'BAD_REQUEST' });
+    //throw new TRPCError({ code: 'BAD_REQUEST' });
 
     return data;
   }),
